1a15ceff594c2df0ab645e71ec0c3f215cc50e18
aartikansal <aartilumb@gmail.com>
2020-02-08 22:59:22 -0500

    changes done on 02/09/2020

--- WriteIFs.java
+++ WriteIFs.java
@@ -2,25 +2,36 @@
 /**
  * Write a description of class WriteIFs here.
  *
- * @author (your name)
- * @version (a version number or a date)
+ * @author (Aarti Kansal)
+ * @version (02/08/2020)
  */
 public class WriteIFs
 {
- 
-    public void playerDied(boolean player1) {
+   int x;
+   int tt_t;
+   int tt_s;
+   int oo1, oo2;
+   String ss;
+ public void playerDied(boolean player1) {
         // Write an IF statement that checks “player1.isAlive()” 
         // and if that’s false, calls “displayGameOver(player1)”
-     
+        if (isAlive(player1)==false) {
+           displayGameOver(player1);
+        }
     }
     
     public String thermoSTAT(int room) {
         // Write an IF statement that checks the 
         // “temperature(room)” and if that check is less than 70, 
         // calls “heatOn()” else calls “coolOn()”
-
-
-        
+        if (tempurature(room)<70)
+        {
+            heatOn();
+        }
+        else
+        {
+            coolOn();
+        }
         return this.ss;
     }
 
@@ -30,14 +41,23 @@ public class WriteIFs
         // AND 
         // “insideTemp()” is less than 62, 
         // calls “startAFire(fireplace1)”
+        if (outsideTemp()<50 & insideTemp()<62){
+           startAFire(fireplace1); 
+        }else
+        {
+            startAFire(fireplace1);
+        }
 
     }
 
-    public void checkFuel(double fuelLevel) {
+     public void checkFuel(double fuelLevel) {
         // Write an IF statement that checks “fuelLevel” 
         // and if that check is less than 0.08, calls “refuel()”
-
+        if (fuelLevel<0.08){
+            refuel();
+        }
     }
+
 
 
     
@@ -47,17 +67,13 @@ public class WriteIFs
      * 
      * instance variables
      * / 
-   int x;
-   int tt_t;
-   int tt_s;
-   int oo1, oo2;
-   String ss;
+   
 
 
   /**
    * Constructor for objects of class WriteIFs
    */
-  public WriteIFs()
+   public WriteIFs()
   {
       // initialise instance variables
       x = 0;
@@ -97,4 +113,5 @@ public class WriteIFs
     private void displayGameOver(boolean b) {
         this.ss = "Game Over!";
     }
+    
 }
